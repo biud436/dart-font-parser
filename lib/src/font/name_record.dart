76bfa9da218422ffb1ed121bfb94f3e2fa805dec
biud436 <biud436@gmail.com>
2022-06-05 22:56:38 +0900

    docs: 이름 테이블에 코멘트 추가

--- lib/src/font/name_record.dart
+++ lib/src/font/name_record.dart
@@ -1,6 +1,6 @@
-/// [NameRecord] 클래스는 이름 테이블에서 사용되는 정보를 가집니다.
-/// 
-/// * [nameID]와 [name]은 중요하게 사용됩니다.
+import 'font.dart' show Font;
+
+/// [NameRecord] 클래스는 [Font] 클래스에서 중요하게 사용됩니다.
 class NameRecord {
   int? platformID = 0;
   int? encodingID = 0;
@@ -10,7 +10,7 @@ class NameRecord {
   int? stringOffset = 0;
   String? name = '';
   int? hexOffset = 0;
-  
+
   NameRecord({
     required this.platformID,
     required this.encodingID,
